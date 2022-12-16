f9922d1799fbb4e82efddc3debcb635bd569726d
KinwaSaladExposed <riseemon@gmail.com>
2022-12-16 18:20:31 +0700

    new commit

--- socialr/src/Data/State.js
+++ socialr/src/Data/State.js
@@ -1,67 +1,76 @@
-let state = {
-Dial:{	
-	Dialogue_Names:[
-  {name:"Mr. Cool",id:1},
-  {name:"Mr. Uncool",id:2},
-  {name:"Mr. Very Cool",id:3}
-],
-	Message_Obj:[
-  
-], newmsg:"hello"
-},
+let store = {
+   _state: {
+    Dial:{  
+        Dialogue_Names:[
+      {name:"Mr. Cool",id:1},
+      {name:"Mr. Uncool",id:2},
+      {name:"Mr. Very Cool",id:3}
+    ],
+        Message_Obj:[
+      
+    ], newmsg:"hello"
+    },
 
-Comment:{
-	PMsg:[
-  {message:"Today I started my post",like:400,id:1},
-  {message:"Yesterday I finished my post",like:2,id:2},
-  {message:"Im taking a break from posting",like:69,id:3}
-],
-newPosts:"hello"
-},
+    Comment:{
+        PMsg:[
+      {message:"Today I started my post",like:400,id:1},
+      {message:"Yesterday I finished my post",like:2,id:2},
+      {message:"Im taking a break from posting",like:69,id:3}
+    ],
+    newPosts:"hello"
+    },
 
-Friends:{
-    FNames:[
-        {name:"John",id:1},
-        {name:"Ronald",id:2},
-        {name:"Frank",id:3}
-    ]
-}
-}
-
-export let createCo = (txt) => {
-    let NewComment = {
-        message:txt,id:4,like:40
+    Friends:{
+        FNames:[
+            {name:"John",id:1},
+            {name:"Ronald",id:2},
+            {name:"Frank",id:3}
+        ]
     }
-    state.Comment.PMsg.unshift(NewComment);
-    state.Comment.newPosts = "";
-    run(state);
-}
-export let postMsg = (text) => {
-    let NewMsg = {
-        message:text,id:1
+    },
+
+    createCo(txt) {
+        let NewComment = {
+            message:txt,id:4,like:40
+        }
+        this._state.Comment.PMsg.unshift(NewComment)
+        this._state.Comment.newPosts = ""
+        this.run(this._state)
+    },
+    postMsg(text) {
+        let NewMsg = {
+            message:text,id:1
+        }
+        this._state.Dial.Message_Obj.push(NewMsg)
+        this.run(this._state)
+    },
+
+    OPC(Otext) {
+        this._state.Comment.newPosts = Otext
+        this.run(this._state)
+    },
+    NM(MMtext) {
+        this._state.Dial.newmsg = MMtext
+        this.run(this._state)
+    },
+
+    
+
+
+    run() {
+        console.log("Test");
+    },
+
+    subscribe(observer) {
+        this.run = observer
+    },
+
+    getState() {
+        return this._state
     }
-    state.Dial.Message_Obj.push(NewMsg);
-    run(state);
+
+    
 }
-
-export let OPC = (Otext) => {
-    state.Comment.newPosts = Otext;
-    run(state);
-}
-export let NM = (MMtext) => {
-    state.Dial.newmsg = MMtext;
-    run(state);
-}
+export default store;
 
 
-
-
-let run = () => {
-    console.log("Test");
-}
-
-export let subscribe = (observer) => {
-    run = observer;
-}
-
-export default state
--- socialr/src/index.js
+++ socialr/src/index.js
@@ -7,7 +7,8 @@ import {createCo} from './Data/State'
 import {postMsg} from './Data/State'
 import {OPC} from './Data/State'
 import {NM} from './Data/State'
-import state from './Data/State'
+import store from './Data/State'
+
 
 
 export let run = (state) => {
@@ -15,17 +16,17 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App 
       state={state}
-      createCo={createCo}
-      postMsg={postMsg}
-      OPC={OPC}
-      NM={NM}
+      createCo={store.createCo.bind(store)}
+      postMsg={store.postMsg.bind(store)}
+      OPC={store.OPC.bind(store)}
+      NM={store.NM.bind(store)}
     />
   </React.StrictMode>
 );
 }
 
-run(state);
-subscribe(run);
+run(store.getState());
+store.subscribe(run);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
